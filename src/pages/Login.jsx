f7debe41d1556c2708d77af0c75310ee0ced174b
Georgiy95 <urijpetrovskij@gmail.com>
2023-10-21 14:10:14 +0300

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { logIn } from 'redux/auth/operations';
 import { selectLoggedIn } from 'redux/auth/selectors';
+import { Link} from 'react-router-dom';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -24,9 +25,17 @@ const Login = () => {
     <form className="form" onSubmit={handleSubmit}>
       <span className="title">Email:</span><input className="input" type="email" name="email" placeholder="email" required />
       <span className="title">Password:</span><input className="input" type="password" name="password" placeholder="password" required />
-      <button className="btn" type="submit">Login</button>
+      <button className="btn" type="submit">Login</button><br /><br />
+
+      <span className="signup">
+          <Link className="home-link" to="/register">Signup 
+          </Link>{' '}if you don't have an account yet
+        </span>
     </form>
+    
   );
 };
 
 export default Login;
+
+
