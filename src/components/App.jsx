8a61b676f2ba300871c9d36fe5ef1ab008a38bd6
Georgiy95 <urijpetrovskij@gmail.com>
2023-10-23 12:53:06 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,53 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import SharedLayout from './SharedLayout';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { refresh } from 'redux/auth/operations';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks';
 
-const Home = lazy(() => import('../pages/Home'));
-const Register = lazy(() => import('../pages/Register'));
-const Login = lazy(() => import('../pages/Login'));
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(refresh());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <b style={{ color: 'white' }}>Refreshing user...</b>
+  ) : (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
   );
--- src/redux/auth/selectors.jsx
+++
@@ -1,3 +0,0 @@
-export const selectLoggedIn = state => state.auth.isLoggedIn;
-export const selectEmail = state => state.auth.userEmail;
-export const selectToken = state => state.auth.token;
